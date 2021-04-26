0ba060abbb8c9a56bad29ab79aeecd2e5d572add
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-04-26 22:11:16 +0900

    04-10テスト作成

--- main.c
+++ main.c
@@ -1,47 +1,13 @@
 #include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    int i, j, k;
-    float answer;
-    float a[3][3], b[3][3], c[3][3];
-    
-    for(i=0; i<3; i++){
-        for(j=0; j<3; j++){
-                scanf("%f", &a[i][j]);
-        }
+#include <string.h>
+int main(){
+    char x[100], y[100];
+    int i;
+    scanf("%s", x);
+    for(i=0; i<strlen(x); i++){
+       y[i]=x[strlen(x)-i-1]; 
     }
-    
-    for(i=0; i<3; ++i){
-        for(j=0; j<3; ++j){
-                scanf("%f", &b[i][j]);
-        }
-    }
-    
-    //行列の積
-    for(i=0; i<3; ++i){
-        for(j=0; j<3; ++j){
-            c[i][j] = a[i][j]*b[i][j];
-            
-            for(i=0; i<3; ++i){
-                for(j=0; j<3; ++j){
-                    answer = 0;
-                    for (k=0; k<3; ++k){
-                        answer = answer + a[i][k]*b[k][j];
-                        c[i][j] = answer;
-                    }
-                }
-            }
-        }
-    }
-    
-    for(i=0; i<3; ++i){
-        for(j=0; j<3; ++j){
-            printf("%f\n",c[i][j]);
-        }
-    }
-    
-    return 0;
+    y[strlen(x)]='\0';
+    strcat(x,y);
+    printf("%s",x);
 }
-
