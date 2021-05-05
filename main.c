6d8cc612caf2bd4bc903169cab34d546103686f3
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-05-05 17:07:20 +0900

    09-16 テスト作成

--- main.c
+++ main.c
@@ -1,57 +1,29 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-/*f(x)の出力*/
-void function()
-{
-    printf("f(x) = pow(x,2) - 2\n");
-}
-
-/*f(x)*/
+/* f(x) */
 double f(double x)
 {
-    return pow(x,2) - 2;
-}
-
-/*f(x)の導関数*/
-double g(double x)
-{
-    return 2 * x;
+    return exp(-x*x);
 }
 
 int main()
 {
-    //利用する変数
-    double x, new_x, eps;
-    int number,i;
+    double a, b, x, h, sum = 0.0;
+    int n, i;
 
-    //データの取り込み
-    function();
-    printf("初期値 : ");
-    scanf("%lf",&x);
-    printf("計算精度 : ");
-    scanf("%lf",&eps);
-    printf("繰り返し上限回数 : ");
-    scanf("%d",&number);
-    printf("繰り返し\tnew x\t\tf(x)\t\tg(x)\n");
-    
-    /* ニュートン法による解の導出 */
-    for(i=0;i<number;i++){
-        new_x = x - f(x) / g(x);
-        /* 収束条件で判定し、解を出力 */
-        if(fabs(new_x - x) < eps && (g(x) > 0.0001)){
-            printf("x = %f\n", new_x);
-            break;
-        }
+    //積分区間
+    scanf("%lf %lf", &a, &b);
+    //分割数
+    scanf("%d", &n);
 
-         printf("%2d\t\t%f\t%f\t%f\n", i, new_x, f(x), g(x));
-
-         /* 収束条件、重解の判定をし、重解を出力 */
-        if((fabs(new_x - x) < eps) && (g(x) <= 0.0001)){
-            printf("x = %f(重解)\n", new_x);
-            break;
-        }
-        x = new_x;
+    // 台形の面積の計算
+    h = (b-a)/n;
+    for (i=0; i<n; i++) {	
+        x = a + (double)i*h;
+        sum += (h/2)*(f(x) + f(x+h));
     }
-    if(i == number) printf("繰り返し上限\n");
+
+    printf("%lf\n",sum);
+
 }
