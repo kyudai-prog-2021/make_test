623ae6f221a6b9d9f679a87c6fae14d9cfc2ed50
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-05-06 14:23:08 +0900

    11-22 マージソートの動作不調

--- main.c
+++ main.c
@@ -1,19 +1,42 @@
 #include<stdio.h>
 #include<math.h>
 
-/* f(x) */
-void bubble(int a[],int left,int right){
-    int i, j, tmp;
-    for(i = left; i < right - 1; i++){
-        for(j = i + 1; j < right; j++){
-            /* もしもa[j]がa[i]より小さいならばa[i]とa[j]を入れ替える */
-            if(a[j] < a[i]){
-                tmp = a[j];
-                a[j] = a[i];
-                a[i] = tmp;
+void merge(int a[],int temp[], int left, int right){
+    int mid, i, j, k;
+    
+    if(left < right){
+        //分割
+        mid = (left + right)/2.0;
+        merge(a, temp,left, mid);
+        merge(a, temp,mid+1, right);
+
+        //前半の代入
+        for(i = left; i <= mid; i++){
+            temp[i] = a[i];
+        }
+        //後半の逆順代入
+        for(j = mid+1; j <= right; j++){
+            temp[right - (j- (mid+1))] = a[j];
+        }
+        
+        i = left;
+        j = right;
+        for(k = left; k <= right; k++){
+            if(temp[i] < temp[j]){
+                a[k] = temp[i++];
+            }
+            else{
+                a[k] = temp[j--];
             }
         }
     }
+
+}
+
+/* マージソート関数 */
+void mergesort(int a[],int left,int right,int N){
+    int mid, i, j, k, temp[N];
+    merge(a,temp,left,right);
 }
 
 int main()
@@ -29,7 +52,7 @@ int main()
         scanf("%d",&a[i]);
     }
 
-    bubble(a,0,N);
+    mergesort(a,0,N,N);
     //ソート結果を出力
     for(i=0;i<N;i++){
         printf("%d\n",a[i]);
