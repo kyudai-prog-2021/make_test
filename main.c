3c33df36355578f840d84da26a9b5f305c3167cf
松尾 春紀 <haruki.matsuo110676@e-seikatsu.co.jp>
2021-05-11 15:02:30 +0900

    11-25 プログラム作成

--- main.c
+++ main.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
-#define M 10
+
+void bubble(int a[],int left,int right){
+    int i, j, tmp;
+    for(i = left; i < right - 1; i++){
+        for(j = i + 1; j < right; j++){
+            /* もしもa[j]がa[i]より小さいならばa[i]とa[j]を入れ替える */
+            if(a[j] < a[i]){
+                tmp = a[j];
+                a[j] = a[i];
+                a[i] = tmp;
+            }
+        }
+    }
+}
 
 void mergesort(int a[], int left, int right,int N){
     int mid,i,j,k,tmp[N];
     if (left >= right) return;
-    
-    //要素を分解
-    mid = (left+right)/2;
-    mergesort(a,left,mid,N);
-    mergesort(a,mid+1,right,N);
+    if (right-left<32){
+        bubble(a,left,right+1);//bubbleのrightは配列の終端より一つ右を指すので，ここで1を加えて補正
+    }
+    else{
+        //要素を分解
+        mid = (left+right)/2;
+        mergesort(a,left,mid,N);
+        mergesort(a,mid+1,right,N);
 
-    //要素を統合
-    for(i=left;i<=mid;i++) tmp[i]=a[i];
-    for(j=mid+1;j<=right;j++) tmp[right-(j-(mid+1))]=a[j];
-    i=left;
-    j=right;
-    for(k=left;k<=right;k++){
-        if(tmp[i]<tmp[j]) a[k]=tmp[i++];
-        else a[k] = tmp[j--];
+        //要素を統合
+        for(i=left;i<=mid;i++) tmp[i]=a[i];
+        for(j=mid+1;j<=right;j++) tmp[right-(j-(mid+1))]=a[j];
+        i=left;
+        j=right;
+        for(k=left;k<=right;k++){
+            if(tmp[i]<tmp[j]) a[k]=tmp[i++];
+            else a[k] = tmp[j--];
+        }
     }
 }
 
